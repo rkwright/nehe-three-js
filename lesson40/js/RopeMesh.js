285630d0cc752c5233c299e3702f7b2616b75940
Ric Wright <rkwright@geofx.com>
2017-05-18 11:37:08 -0500

    Modified the ropeMesh class to create a mesh aligned to a vector, and added a method to update the alignment of the cylinder to new vector coordinates.

--- lesson40/js/RopeMesh.js
+++ lesson40/js/RopeMesh.js
@@ -11,7 +11,7 @@ GFX.RopeMesh = function () {
 
 GFX.RopeMesh.prototype = {
 
-    alignCylinder: function ( point0, point1, material ) {
+    createCylinder: function ( point0, point1, diameter, material ) {
         this.direction.subVectors(point1, point0);
         this.orientation.lookAt(point0, point1, this.threeUp);
 
@@ -20,7 +20,7 @@ GFX.RopeMesh.prototype = {
                          0, -1, 0, 0,
                          0,  0, 0, 1 );
         this.orientation.multiply(this.matrix);
-        var cylinderGeom = new THREE.CylinderGeometry(0.5, 0.5, this.direction.length(), 8, 1);
+        var cylinderGeom = new THREE.CylinderGeometry(diameter, diameter, this.direction.length(), 8, 1);
         var cylinderMesh = new THREE.Mesh( cylinderGeom, material );
         cylinderMesh.applyMatrix(this.orientation);
 
@@ -30,6 +30,27 @@ GFX.RopeMesh.prototype = {
         cylinderMesh.position.z = (point1.z + point0.z) / 2;
 
         return cylinderMesh;
+    },
+
+    alignCylinder: function ( point0, point1, cylinderMesh ) {
+        this.direction.subVectors(point1, point0);
+        this.orientation.lookAt(point0, point1, this.threeUp);
+
+        this.matrix.set( 1,  0, 0, 0,
+            0,  0, 1, 0,
+            0, -1, 0, 0,
+            0,  0, 0, 1 );
+        this.orientation.multiply(this.matrix);
+        cylinderMesh.matrix.identity();
+        cylinderMesh.applyMatrix(this.orientation);
+
+        // position based on midpoints - there may be a better solution than this
+        cylinderMesh.position.x = (point1.x + point0.x) / 2;
+        cylinderMesh.position.y = (point1.y + point0.y) / 2;
+        cylinderMesh.position.z = (point1.z + point0.z) / 2;
+
+        cylinderMesh.geometry.verticesNeedUpdate = true;
+
     }
 };
 
